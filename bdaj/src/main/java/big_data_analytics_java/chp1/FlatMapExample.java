99cca7b914ca91c40fffc164e3e90f83b4f30f26
Ilya <ilya1245@gmail.com>
2019-01-28 20:13:52 +0200

    RDD actions 1

--- bdaj/src/main/java/big_data_analytics_java/chp1/FlatMapExample.java
+++ bdaj/src/main/java/big_data_analytics_java/chp1/FlatMapExample.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -16,6 +18,7 @@ public class FlatMapExample {
 	private static String FILE_NAME = "data/university-rankings/school_and_country_table.csv";
 	
 	public static void main(String[] args) {
+		LogManager.getLogger("org").setLevel(Level.OFF);
 		SparkConf conf = new SparkConf().setAppName(appName).setMaster(master);
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		JavaRDD<String> rddX = sc.parallelize(
@@ -23,8 +26,10 @@ public class FlatMapExample {
 		 JavaRDD<String[]> rddY = rddX.map(e -> e.split(" "));
          JavaRDD<String> rddY2 = rddX.flatMap(e -> Arrays.asList(e.split(" ")).iterator());
 	        List<String> listUsingFlatMap = rddY2.collect();
-	        
-		
+	        System.out.println(listUsingFlatMap);
+		System.out.println("-----------------------------");
+		rddY2 = rddY.flatMap(e -> Arrays.asList(e).iterator());
+		System.out.println(rddY2.collect());
 	}
 
 }
--- bdaj/src/main/java/big_data_analytics_java/chp1/LoadData.java
+++ bdaj/src/main/java/big_data_analytics_java/chp1/LoadData.java
@@ -13,31 +13,32 @@ import org.apache.spark.api.java.function.FlatMapFunction;
 
 public class LoadData {
 
-	private static String appName = "LOAD_DATA_APPNAME";
-	private static String master = "local";
-	private static String FILE_NAME = "data/university-rankings/school_and_country_table.csv";
-	
-	public static void main(String[] args) {
-		LogManager.getLogger("org").setLevel(Level.OFF);
-		SparkConf conf = new SparkConf().setAppName(appName).setMaster(master);
-		JavaSparkContext sc = new JavaSparkContext(conf);
-		JavaRDD<String> rowRdd = sc.textFile(FILE_NAME);
-			System.out.println(rowRdd.count());
-		JavaRDD arr = rowRdd.flatMap(x -> Arrays.asList(x.split(",")).iterator());
-		List<String> l = arr.take(3);
-			for (String object : l) {
-				System.out.println(object);
-				System.out.println("--------------------");
-			}
-		JavaRDD<String> filteredRows = rowRdd.filter(s -> s.contains("Santa Barbara"));
-			System.out.println(filteredRows.count());
-		JavaRDD<Integer> rowlengths = rowRdd.map(s -> s.length());
-		List<Integer> rows = rowlengths.collect();
-		for (Integer row : rows) {
-			System.out.println(row);
-		}
+    private static String appName = "LOAD_DATA_APPNAME";
+    private static String master = "local";
+    private static String FILE_NAME = "data/university-rankings/school_and_country_table.csv";
+
+    public static void main(String[] args) {
+        LogManager.getLogger("org").setLevel(Level.OFF);
+        SparkConf conf = new SparkConf().setAppName(appName).setMaster(master);
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> strRowRdd = sc.textFile(FILE_NAME);
+        System.out.println(strRowRdd.count());
+        JavaRDD arr = strRowRdd.flatMap(x -> Arrays.asList(x.split(",")).iterator());
+        List<String> l = arr.take(3);
+        for (String object : l) {
+            System.out.println(object);
+            System.out.println("--------------------");
+        }
+        JavaRDD<String> filteredRows = strRowRdd.filter(s -> s.contains("Santa Barbara"));
+        System.out.println("filteredRows.count " + filteredRows.count());
+        System.out.println("filteredRows " + filteredRows.take(1));
+        JavaRDD<Integer> rowlengths = strRowRdd.map(s -> s.length());
+        /*List<Integer> rows = rowlengths.collect();
+        for (Integer row : rows) {
+            System.out.println(row);
+        }*/
 
 
-	}
+    }
 
 }
