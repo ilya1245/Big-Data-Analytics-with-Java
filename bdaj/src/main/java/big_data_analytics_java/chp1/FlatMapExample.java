ee461dd04d96a9c739dce4be9dfcb702a45e13fd
Ilya <ilya1245@gmail.com>
2019-01-29 00:00:27 +0200

    Data files have been moved

--- bdaj/src/main/java/big_data_analytics_java/chp1/FlatMapExample.java
+++ bdaj/src/main/java/big_data_analytics_java/chp1/FlatMapExample.java
@@ -15,7 +15,7 @@ public class FlatMapExample {
 
 	private static String appName = "LOAD_DATA_APPNAME";
 	private static String master = "local";
-	private static String FILE_NAME = "data/university-rankings/school_and_country_table.csv";
+	private static String FILE_NAME = "resources/data/university-rankings/school_and_country_table.csv";
 	
 	public static void main(String[] args) {
 		LogManager.getLogger("org").setLevel(Level.OFF);
