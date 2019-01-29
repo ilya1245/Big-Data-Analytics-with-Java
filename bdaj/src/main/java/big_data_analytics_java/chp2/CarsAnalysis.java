d65aca30476da519a88c14e4f5785196e1772d6b
Ilya <ilya1245@gmail.com>
2019-01-30 01:35:51 +0200

    csv reading works!

--- bdaj/src/main/java/big_data_analytics_java/chp2/CarsAnalysis.java
+++ bdaj/src/main/java/big_data_analytics_java/chp2/CarsAnalysis.java
@@ -6,22 +6,34 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
 
 public class CarsAnalysis {
     public static void main(String[] args) {
         LogManager.getLogger("org").setLevel(Level.OFF);
 
-        SparkConf c = new SparkConf().setMaster("local[*]");
+        SparkConf c = new SparkConf()
+                .setMaster("local[*]")
+                .setAppName("CarsAnalysis")
+                .set("spark.driver.allowMultipleContexts", "true");
 
         SparkSession spark = SparkSession
                 .builder()
                 .config(c)
-                .appName("LoanDefaultPrediction")
+                .appName("CarsAnalysis")
                 .getOrCreate();
 
-
+        System.out.println("---------- carsBaseDF ----------");
         Dataset<Row> carsBaseDF = spark.read().json("resources/data/cars/cars.json");
+        carsBaseDF.show();
+
+        System.out.println("---------- carsBaseDF1 ----------");
+        //SparkConf conf = new SparkConf().setAppName(appName).setMaster(master);
+        JavaSparkContext sc = new JavaSparkContext(c);
+        SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
+        Dataset<Row> carsBaseDF1 = sqlContext.read().json("resources/data/cars/cars.json");
+        carsBaseDF1.show();
     }
 
 }
